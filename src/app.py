680f87c6ee38e4ead406591e8a86cb06a293de49
Better-than-Elon <arkadyartemenko@gmail.com>
2024-04-09 02:20:44 +0300

    add api

---
+++ src/app.py
@@ -0,0 +1,22 @@
+import configparser
+from flask import Flask
+from predict import Predictor
+
+app = Flask(__name__)
+predictor = None
+
+
+@app.route("/predict/<pred_id>")
+def get_predict(pred_id):
+    X_test = predictor.X_test.iloc[int(pred_id):int(pred_id) + 1]
+    y_test = predictor.y_test.iloc[int(pred_id), 0]
+    y_pred = predictor.clf.predict(X_test)[0]
+    return f'Data:{X_test.iloc[0]} Pred: {y_pred} Real: {y_test}', 200
+
+
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    predictor = Predictor.from_config(config)
+
+    app.run(debug=True)
